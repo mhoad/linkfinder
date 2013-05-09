a05f16b328f9ce69e79928083b8406783f75eca5
Mark Hoad <markdhoad@gmail.com>
2013-05-10 01:48:44 +1000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linkfinder::Application.config.secret_token = '4d9ae0f12b7514f064dd1e1564e269ed90af07639e5c6fc9ac5401ea9ed6dced8b27deb0e49f5dfeededbc25300f24b4527fd4dbe4734df4a0b1c5904ca71063'
